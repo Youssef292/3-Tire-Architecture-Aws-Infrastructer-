7174e455dc00f75afce6e90b976fc27e6f6a1401
Youssef Abdelhakem <70919183+Youssef292@users.noreply.github.com>
2024-09-22 00:59:32 +0300

    Update load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "jenkins_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = var.public_subnets
+  subnets            = public_subnets 
 
   tags = {
     Environment = "production"
@@ -14,11 +14,11 @@ resource "aws_lb_target_group" "jenkins_tg" {
   name        = "jenkins-tg"
   port        = 8080
   protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+  vpc_id      = vpc_id 
   target_type = "instance"
 
   health_check {
-    path = "/"  # Adjust if Jenkins health check path is different
+    path = "/"  
   }
 }
 
@@ -34,16 +34,16 @@ resource "aws_lb_listener" "jenkins_listener" {
 }
 
 resource "aws_lb_target_group_attachment" "jenkins" {
-  count            = var.jenkins_instances
+  count            = length(jenkins_instances_count) 
   target_group_arn = aws_lb_target_group.jenkins_tg.arn
-  target_id        = module.instances.jenkins_instances[count.index].id
+  target_id        = jenkins_instances_count[count.index].id 
   port             = 8080
 }
 
 resource "aws_security_group" "alb_sg" {
   name        = "alb_sg"
   description = "Allow inbound HTTP"
-  vpc_id      = variable.vpc_id
+  vpc_id      = vpc_id
 
   ingress {
     from_port   = 80
