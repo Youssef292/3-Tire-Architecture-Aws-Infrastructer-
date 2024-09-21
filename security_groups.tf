f9cab1c7e33e30ba29d4ad4f37e174a7b9b352fc
Youssef Abdelhakem <70919183+Youssef292@users.noreply.github.com>
2024-09-21 23:37:59 +0300

    Add files via upload

---
+++ security_groups.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "public_security_group" {
+  name = "public-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_security_group" {
+  name = "private-security-group"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
